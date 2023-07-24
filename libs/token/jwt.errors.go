0cabc86ea975c37030f95bec0be93d2b1592738c
codefresco <hamid.tarmazdi@gmail.com>
2023-07-24 23:53:03 +0930

    Added jwt toekn validation

---
+++ libs/token/jwt.errors.go
@@ -0,0 +1,7 @@
+package jwt
+
+import "errors"
+
+var (
+	ErrUnauthorized = errors.New("Unauthorized!")
+)
--- libs/token/jwt.go
+++ libs/token/jwt.go
@@ -8,10 +8,12 @@ import (
 	"github.com/google/uuid"
 )
 
+type MapClaims = jwt.MapClaims
+
 func generateJWT(subject string, ttl time.Duration) (string, error) {
 	configs := config.GetConfig()
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, MapClaims{
 		"sub":  subject,
 		"exp":  time.Now().Add(ttl).Unix(),
 		"iat":  time.Now().Unix(),
@@ -38,4 +40,25 @@ func GenerateRefreshToken(email string) (string, error) {
 	signedToken, err := generateJWT(email, configs.RefreshTokenExpiry)
 
 	return signedToken, err
+}
+
+func ValidateToken(jwtToken string) (MapClaims, error) {
+	configs := config.GetConfig()
+
+	token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+		if _, hmac := token.Method.(*jwt.SigningMethodHMAC); !hmac {
+			return nil, ErrUnauthorized
+		}
+		return []byte(configs.JWTSecret), nil
+	})
+
+	if err != nil {
+		return nil, ErrUnauthorized
+	}
+
+	if claims, ok := token.Claims.(MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, ErrUnauthorized
 }
